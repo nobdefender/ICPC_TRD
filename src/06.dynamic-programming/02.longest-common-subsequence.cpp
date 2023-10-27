96745fceac034bfc324e71ccf64244ad8cd44201
Lev Provotorin <robotolev@yandex.ru>
2023-10-27 11:18:33 +0300

    Upload dp for LCS

---
+++ src/06.dynamic-programming/02.longest-common-subsequence.cpp
@@ -0,0 +1,29 @@
+// Theme: Longest Common Subsequence
+
+// Algorithm: LCS via simple dynamic
+// Complexity: O(n*m)
+
+vector<int> lis(vector<int>& arr) {
+    int n = arr.size();
+    vector<int> dp(n + 1, INF); dp[0] = -INF;
+    vector<int> pos(n + 1, -1), previous(n, -1);
+    int length = 0;
+    for (int i = 0; i < n; ++i) {
+        int j = lower_bound(dp.begin(), dp.end(), arr[i]) - dp.begin();
+        if (dp[j - 1] < arr[i] && arr[i] < dp[j]) {
+            dp[j] = arr[i];
+            pos[j] = i;
+            previous[i] = pos[j - 1];
+            length = max(length, j);
+        }
+    }
+
+    vector<int> res;
+    int p = pos[length];
+    while (p != -1) {
+        res.push_back(arr[p]);
+        p = previous[p];
+    }
+    reverse(res.begin(), res.end());
+    return res;
+}
