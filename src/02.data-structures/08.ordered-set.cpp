4b9156b20ce2cc6d77a851dfdcf6223346e1f6dc
RobotoLev <robotolev@yandex.ru>
2024-04-13 18:15:11 +0300

    Fix ordered_set

--- src/02.data-structures/08.ordered-set.cpp
+++ src/02.data-structures/08.ordered-set.cpp
@@ -1,16 +1,20 @@
-#include <ext/pb_ds/detail/standard_policies.hpp>
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
 
 typedef tree<
 int,
-null_type, // или тип значения, если не set, а map хотим
-less<int>,
+null_type, // or value type, if we want map instead of set
+less<>, // or less_equal, if we want multi-version; also greater or something
 rb_tree_tag,
 tree_order_statistics_node_update>
 ordered_set;
 
-// usage 
+// usage
 ordered_set X;
-X.insert(1); // итератор на k-й по величине элемент (отсчет с нуля)
+X.insert(1);
 X.insert(2);
-cout << *X.find_by_order(1) << endl; // количество элементов, строго меньших данного
-cout << X.order_of_key(-5) << endl;
+cout << *X.find_by_order(1) << endl; // iterator to k-th by value element, starting on zero
+cout << X.order_of_key(-5) << endl; // a number of elements strictly less than given
