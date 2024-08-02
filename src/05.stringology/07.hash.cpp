7089e7b673cc71cd7dd9a06f3c1b3894211ecb1d
Lev Provotorin <robotolev@yandex.ru>
2024-08-02 15:27:19 +0300

    Change string hash implementation

--- src/05.stringology/07.hash.cpp
+++ src/05.stringology/07.hash.cpp
@@ -1,87 +1,74 @@
 // Algorithm: String hash
 
-#define forn(i, n) for (int i = 0; i < (int)(n); i++)
-#define sz(a) (int)(a).size()
+#define int long long
 
-typedef long long ll;
-typedef unsigned long long ull;
+struct Hasher {
+    int n;
+    int P1, MOD1;
+    int P2, MOD2;
+    vector<string> str;
+    vector<int> p1, p2;
+    vector<vector<int>> h1, h2;
 
-struct num {
-  static const int MA = 1e9 + 7, MB = 1e9 + 9;
-
-  int a, b;
-
-  num() {}
-  num(int x) : a(x), b(x) {}
-  num(int a, int b) : a(a), b(b) {}
-
-  num operator+(const num &x) const { return num((a + x.a) % MA, (b + x.b) % MB); }
-  num operator-(const num &x) const { return num((a + MA - x.a) % MA, (b + MB - x.b) % MB); }
-  num operator*(int x) const { return num(((ll)a * x) % MA, ((ll)b * x) % MB); }
-  num operator*(const num &x) const { return num(((ll)a * x.a) % MA, ((ll)b * x.b) % MB); }
-  bool operator==(const num &x) const { return a == x.a && b == x.b; }
-
-  explicit operator ll() const { return (ll)a * MB + b + 1; } // > 0
-};
-
-template <class hash_t>
-struct StrComparator {
-  static const int P;
-  static vector<hash_t> deg;
-
-  int n;
-  const char *s;
-  hash_t *h;
-
-  StrComparator(int n, const char *s) : n(n), s(s) {
-    h = new hash_t[n + 1];
-    h[0] = 0;
-    forn(i, n)
-        h[i + 1] = h[i] * P + s[i];
-    deg.reserve(n);
-    while (sz(deg) <= n)
-      deg.push_back(*deg.rbegin() * P);
-  }
-
-  hash_t substr(int i, int len) const { return h[i + len] - h[i] * deg[len]; }
-
-  int lcp(int i, int j) {
-    int L = 0, R = n - max(i, j);
-    while (L < R) {
-      int M = (L + R + 1) / 2;
-      if (substr(i, M) == substr(j, M))
-        L = M;
-      else
-        R = M - 1;
+    Hasher(int n, int P1, int MOD1, int P2, int MOD2): n(n), P1(P1), MOD1(MOD1), P2(P2), MOD2(MOD2), p1(n + 1, 1), p2(p1) {
+        for (int i = 1; i <= n; ++i) {
+            p1[i] = p1[i - 1] * P1 % MOD1;
+            p2[i] = p2[i - 1] * P2 % MOD2;
+        }
     }
-    return L;
-  }
 
-  int cmp(int a, int b) {
-    int LEN = n - max(a, b), L = lcp(a, b);
-    return L < LEN ? (int)s[a + L] - s[b + L] : b - a;
-  }
+    pair<int, int> calc(const string& s) const {
+        pair<int, int> res = {0, 0};
+        for (auto& c: s) {
+            res.first = (res.first * P1 + (c + 1)) % MOD1;
+            res.second = (res.second * P2 + (c + 1)) % MOD2;
+        }
+        return res;
+    }
 
-  bool operator()(int i, int j) { return cmp(i, j) < 0; }
+    void add(const string& s) {
+        assert((int)s.size() <= n);
+        int j = (int)h1.size();
+        str.push_back(s);
+        h1.push_back(vector<int>(n + 1, 0));
+        h2.push_back(h1.back());
+        for (int i = 1; i <= (int)s.size(); ++i) {
+            h1[j][i] = (h1[j][i - 1] * P1 + (s[i - 1] + 1)) % MOD1;
+            h2[j][i] = (h2[j][i - 1] * P2 + (s[i - 1] + 1)) % MOD2;
+        }
+    }
+
+    // 0-indexation here
+    [[nodiscard]] pair<int, int> get(int idx, int left, int right) const {
+        return {
+                (h1[idx][right + 1] - h1[idx][left] * p1[right + 1 - left] % MOD1 + MOD1) % MOD1,
+                (h2[idx][right + 1] - h2[idx][left] * p2[right + 1 - left] % MOD2 + MOD2) % MOD2
+        };
+    }
+
+    int max_common_prefix(int idx1, int left1, int right1, int idx2, int left2, int right2) const {
+        int left = 0, right = min(right1 - left1, right2 - left2) + 2;
+        while (right - left > 1) {
+            int mid = (left + right) / 2;
+            if (get(idx1, left1, left1 + mid - 1) == get(idx2, left2, left2 + mid - 1)) left = mid;
+            else right = mid;
+        };
+        return left;
+    }
+
+    int max_common_suffix(int idx1, int left1, int right1, int idx2, int left2, int right2) const {
+        int left = 0, right = min(right1 - left1, right2 - left2) + 2;
+        while (right - left > 1) {
+            int mid = (left + right) / 2;
+            if (get(idx1, right1 - mid + 1, right1) == get(idx2, right2 - mid + 1, right2)) left = mid;
+            else right = mid;
+        }
+        return left;
+    }
+
+    bool compare_less(int idx1, int left1, int idx2, int left2, int sz) const {
+        int mx = max_common_prefix(idx1, left1, left1 + sz - 1, idx2, left2, left2 + sz - 1);
+        if (mx == sz) return false;
+        return str[idx1][left1 + mx] < str[idx2][left2 + mx];
+    }
 };
-template <class hash_t>
-vector<hash_t> StrComparator<hash_t>::deg(1, hash_t(1));
-template <class hash_t>
-const int StrComparator<hash_t>::P = max(239, rand());
-
-/*
-    std::string s = "Hello World!";
-    const int length = s.length();
-    char* char_array = new char[length + 1];
-    strcpy(char_array, s.c_str());
-*/
-
-/**
- * Usage:
- *   StrComparator<num> h(length, s); // int length, char *s
- *   h.substr(0, 3) == h.substr(1, 3); // сравнение на равенство подстрок за O(1)
- *   h.cmp(2, 3); // сравнение на больше-меньше суффиксов за O(logn)
- *
- *   int p[n]; forn(i, n) p[i] = i;
- *   sort(p, p + n, h); // сортировать суффиксы, суф.массив за O(nlog^2n)
- */
